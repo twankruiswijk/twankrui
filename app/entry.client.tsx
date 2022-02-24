b0a99e9520d117c94ae403308eff0576f4ef6dfd
Twan Kruiswijk <twankrui@gmail.com>
2022-02-24 14:50:18 +0100

    Add fathom analytics

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,12 @@
-import { hydrate } from "react-dom";
-import { RemixBrowser } from "remix";
+import { hydrate } from 'react-dom';
+import { RemixBrowser } from 'remix';
+import { load } from 'fathom-client';
 
 hydrate(<RemixBrowser />, document);
+
+if (process.env.NODE_ENV !== 'development') {
+  load('EOCGSHWX', {
+    spa: 'history',
+    excludedDomains: ['localhost'],
+  });
+}
